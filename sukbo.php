043797bd0a1d241797c61e42da15b69c14009892
Ashan Perera <ashantiwankaperera@gmail.com>
2022-01-14 22:44:43 +0530

    request2

--- sukbo.php
+++ sukbo.php
@@ -1,15 +1,7 @@
 <?php
+
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-
-echo ($db["host"],$db["port"],$db["user"],$db["pass"]);
+echo ($db["host"]."///".$db["port"]."///".$db["user"]."///".$db["pass"]);
 
 ?>
